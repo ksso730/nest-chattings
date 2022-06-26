14ae5cb30d59f293b18654f3825f14da28f5561f
ksso730 <ksso7300@gmail.com>
2022-06-26 17:32:43 +0900

    lifecycle hooks 사용해보기

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -2,12 +2,33 @@ import { Socket } from 'socket.io';
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway implements OnGatewayInit {
+  private logger = new Logger('chat');
+
+  constructor() {
+    this.logger.log('constructor');
+  }
+  // afterInit() :gateway가 실행될 때, constructor 다음으로 가장먼저 실행됨
+  afterInit() {
+    this.logger.log('init');
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`disconneceted ${socket.id} ${socket.nsp.name}`);
+    // LOG [chat] disconneceted gRu5SBIZMmkTWPmWAAAD /chattings
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    this.logger.log(`conneceted ${socket.id} ${socket.nsp.name}`);
+    // LOG [chat] conneceted gRu5SBIZMmkTWPmWAAAD /chattings
+  }
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // 현재 폴더의 상위../public
+  // 현재 폴더(__dirname: src) 의 상위 ../public
   app.useStaticAssets(join(__dirname, '..', 'public'));
   // static asset을 public으로 지정. nest에서 자동으로 public 하위 디렉토리들을 찾아준다.
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
